cfa69c5b01a1463115b33665abba7f0cbb5e161c
Anufriyeva <126349829+Anufriyeva@users.noreply.github.com>
2023-10-09 20:13:43 +0300

    Update Movies.jsx

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { searchMovies } from '../../Service/Service';
 import { MoviesContainer, SearchForm, MovieList, MovieCard, MovieImage } from './Movies.styled';
 
@@ -7,23 +7,33 @@ import { MoviesContainer, SearchForm, MovieList, MovieCard, MovieImage } from '.
 const Movies = () => {
   const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const queryValue = new URLSearchParams(location.search).get('query');
 
     useEffect(() => {
-  async function fetchData() {
-    if (query.trim() !== '') {
-      try {
-        const searchResults = await searchMovies(query);
-        setMovies(searchResults.results);
-      } catch (error) {
-        console.error('Error occurred while loading movies:', error);
-      }
-    } else {
-      setMovies([]);
+    if (queryValue) {
+      setQuery(queryValue);
     }
-  }
+  }, [queryValue]);
 
-  fetchData();
-}, [query]);
+    useEffect(() => {
+    async function fetchData() {
+      if (query.trim() !== '') {
+        try {
+          const searchResults = await searchMovies(query);
+          setMovies(searchResults.results);
+        } catch (error) {
+          console.error('Error occurred while loading movies:', error);
+        }
+      } else {
+        setMovies([]);
+      }
+    }
+
+    fetchData();
+  }, [query]);
     
     const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -31,6 +41,9 @@ const Movies = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (query.trim() !== '') {
+      navigate(`/movies?query=${query}`);
+    }
   };
 
   return (
@@ -47,15 +60,15 @@ const Movies = () => {
       </SearchForm>
       <MovieList>
         {movies.map((movie) => (
-          <MovieCard key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>
-              <MovieImage
-                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <h2>{movie.title}</h2>
-            </Link>
-          </MovieCard>
+            <MovieCard key={movie.id}>
+                <Link to={`/movies/${movie.id}`}>
+                    <MovieImage
+                        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                        alt={movie.title}
+                    />
+                    <h2>{movie.title}</h2>
+                </Link>
+            </MovieCard>
         ))}
       </MovieList>
     </MoviesContainer>
